8b43c15b9d3ce059f9f73112f49356181fb2e89c
Guilherme Bermêo <gm@bermeo.dev>
2022-06-29 20:59:41 -0300

    📖 0210 Componentes

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,58 +1,14 @@
 import React from "react";
-
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-    { nome: "Guitarra", preco: "R$ 3500" },
-  ],
-  ativa: false,
-};
+import Footer from "./components/Footer";
+import Form from "./components/Form";
+import Header from "./components/Header";
 
 const App = () => {
-  const dados = luana;
-  let totalGasto = 0;
-
-  const active = {
-    color: "green",
-  };
-  const inactive = {
-    color: "tomato",
-  };
-
-  for (let i = 0; i < dados.compras.length; i++) {
-    totalGasto += parseInt(dados.compras[i].preco.replace("R$ ", ""));
-  }
-
   return (
     <>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:
-        <span style={dados.ativa ? active : inactive}>
-          {dados.ativa ? "Ativa" : "Inativa"}
-        </span>
-      </p>
-      <p>Total gasto: R$ {totalGasto}</p>
+      <Header />
+      <Form />
+      <Footer />
     </>
   );
 };
