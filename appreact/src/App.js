5609f7b7bf709651faee1409042bbe463adca3f3
Guilherme Bermêo <gm@bermeo.dev>
2022-07-03 20:20:14 -0300

    📖 0302 useEffect 1

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,39 +1,27 @@
 import React from "react";
-import Produto from "./Produto";
-import Button from "./Components/Button";
-
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
 
 const App = () => {
+  const [contar, setContar] = React.useState(0);
   const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(false);
 
-  async function fetchProduto(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
-    );
-    const json = await response.json();
-    setDados(json);
-    console.log(json);
-    setCarregando(false);
-  }
+  React.useEffect(() => {
+    // se o fetch estivesse fora do useEffect, toda vez que o componente
+    // fosse atualizado, o mesmo seria executado
+    fetch("https://ranekapi.origamid.dev/json/api/produto/notebook")
+      .then((response) => response.json())
+      .then((json) => setDados(json));
+  }, []);
 
   return (
-    <>
-      <Button onClick={fetchProduto} texto="tablet" />
-      <Button onClick={fetchProduto} texto="smartphone" />
-      <Button onClick={fetchProduto} texto="notebook" />
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produto dados={dados} />}
-    </>
+    <div>
+      {dados && (
+        <div>
+          <h1>{dados.nome}</h1>
+          <p>R$ {dados.preco * contar}</p>
+        </div>
+      )}
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+    </div>
   );
 };
 
--- appreact/src/Components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ texto }) => {
-  return <button style={{ margin: ".5rem" }}>{texto}</button>;
-};
-
-export default Button;
