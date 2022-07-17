7793227b728c2012bc8028a61d69e63ff3cb675c
Guilherme Bermêo <gm@bermeo.dev>
2022-07-17 16:29:37 -0300

    📖 0412 Desafio Formulários (Exercício)

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,111 +1,74 @@
 import React from "react";
-import useForm from "./Hooks/useForm";
-import Checkbox from "./Form/Checkbox";
-import Input from "./Form/Input";
-import Radio from "./Form/Radio";
-import Select from "./Form/Select";
+import Pergunta from "./Components/Pergunta";
 
 const App = () => {
-  const componentesOptions = [
-    "Camera",
-    "Som",
-    "Bluetooth",
-    "Luz",
-    "GPS",
-    "NFC",
-    "Wi-Fi",
-  ];
-  const cores = [
-    "Vermelho",
-    "Azul",
-    "Verde",
-    "Amarelo",
-    "Laranja",
-    "Roxo",
-    "Branco",
+  const [numeroPergunta, setNumeroPergunta] = React.useState(0);
+  const [resposta, setResposta] = React.useState(null);
+  const [pontuacao, setPontuacao] = React.useState(0);
+
+  const perguntas = [
+    {
+      pergunta: "Qual método é utilizado para criar componentes?",
+      options: [
+        "React.makeComponent()",
+        "React.createComponent()",
+        "React.createElement()",
+      ],
+      resposta: "React.createElement()",
+      id: "p1",
+    },
+    {
+      pergunta: "Como importamos um componente externo?",
+      options: [
+        'import Component from "./Component"',
+        'require("./Component")',
+        'import "./Component"',
+      ],
+      resposta: 'import Component from "./Component"',
+      id: "p2",
+    },
+    {
+      pergunta: "Qual hook não é nativo?",
+      options: ["useEffect()", "useFetch()", "useCallback()"],
+      resposta: "useFetch()",
+      id: "p3",
+    },
+    {
+      pergunta: "Qual palavra deve ser utilizada para criarmos um hook?",
+      options: ["set", "get", "use"],
+      resposta: "use",
+      id: "p4",
+    },
   ];
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (nome.validate() && cep.validate() && email.validate()) {
-      console.log("Enviar");
-    } else {
-      console.log("Não enviar");
+  function proximaPergunta() {
+    verificaResposta();
+    setNumeroPergunta(numeroPergunta + 1);
+    setResposta(null);
+  }
+
+  function verificaResposta() {
+    if (resposta === perguntas[numeroPergunta].resposta) {
+      setPontuacao(pontuacao + 1);
     }
   }
 
-  const nome = useForm();
-  const sobrenome = useForm(false);
-  const email = useForm("email");
-  const cep = useForm("cep");
-  const [produto, setProduto] = React.useState("");
-  const [marca, setMarca] = React.useState("");
-  const [cor, setCor] = React.useState("Branco");
-  const [componentes, setComponentes] = React.useState([]);
-  const [termos, setTermos] = React.useState([]);
-
   return (
     <>
-      <h1>Inputs</h1>
-      <form onSubmit={handleSubmit}>
-        <h2>Comprar</h2>
-        <Input label="Nome" id="nome" value={nome} {...nome} />
-        <Input
-          label="Sobrenome"
-          id="sobrenome"
-          value={sobrenome}
-          {...sobrenome}
-        />
-        <Input label="E-mail" id="email" type="email" {...email} />
-        <Input
-          label="CEP"
-          id="CEP"
-          placeholder="00000-000"
-          maxLength={9}
-          {...cep}
-        />
+      {numeroPergunta <= perguntas.length - 1 ? (
+        <>
+          <Pergunta
+            perguntas={perguntas}
+            numeroPergunta={numeroPergunta}
+            resposta={resposta}
+            setResposta={setResposta}
+          />
 
-        <h2>Produto</h2>
-        <Select
-          options={["Smartphone", "Tablet", "Smartspeaker"]}
-          value={produto}
-          setValue={setProduto}
-        />
-
-        <h2>Marca</h2>
-        <Select
-          options={["Xing-Ling", "Null Phone", "Gettygetty", "Hongue Kongue"]}
-          value={marca}
-          setValue={setMarca}
-        />
-
-        <h2>Cor</h2>
-        <Radio options={cores} value={cor} setValue={setCor} />
-
-        <h2>Componentes</h2>
-        <Checkbox
-          options={componentesOptions}
-          value={componentes}
-          setValue={[setComponentes]}
-        />
-
-        {produto && (
-          <h4>
-            Você escolheu o {produto} {cor && cor}
-            {marca && " da marca " + marca}
-            {componentes.length > 0 && " com " + componentes.join(", ")}.
-          </h4>
-        )}
-
-        <h2>Termos de uso</h2>
-        <Checkbox
-          options={["Li e aceito os termos"]}
-          value={termos}
-          setValue={setTermos}
-        />
-      </form>
-      <br />
-      {termos.length > 0 && <button>Comprar</button>}
+          {resposta && <button onClick={proximaPergunta}>Próxima</button>}
+        </>
+      ) : (
+        <h1>Você acertou {pontuacao} perguntas.</h1>
+      )}
     </>
   );
 };
