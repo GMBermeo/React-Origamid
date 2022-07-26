c54ff288a5249b21f853f4f435a6121e14b957be
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-07-26 17:22:42 -0300

    📖 0701 PropTypes

--- appreact/src/pages/_app.js
+++ appreact/src/pages/_app.js
@@ -1,30 +1,12 @@
 import React from "react";
-import "../assets/animations.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../components/ui/Header/Header";
-import Footer from "../components/ui/Footer/Footer";
-import Produtos from "./produtos";
-import Contato from "./contato";
-import Produto from "./produto/produto";
-import NotFounded from "./notFounded";
+import Button from "../components/ui/button";
 
-const App = () => {
+const _app = () => {
   return (
-    <>
-      <div className="container mx-auto flex min-h-screen flex-1 flex-col px-5">
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Produtos />} />
-            <Route path="contato" element={<Contato />} />
-            <Route path="produto/:id" element={<Produto />} />
-            <Route path="*" element={<NotFounded />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      <Footer />
-    </>
+    <div>
+      <Button width={300}>Clique aqui</Button>
+    </div>
   );
 };
 
-export default App;
+export default _app;
